becbef47855d431121441ea27d6cd061377e2a2a
Vyacheslav Anishchenko <avuua16@gmail.com>
2024-04-22 00:22:27 +0200

    Implemented storing of JWT token in cookies and automatic attaching of it to requests to server

--- ForumProject/ForumProject/settings.py
+++ ForumProject/ForumProject/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.middleware.JWTAuthMiddleware',
 ]
 
 ROOT_URLCONF = 'ForumProject.urls'
