bc551187cdddbba490a7d00716a4875251de99f6
Andrewakiv <danchyshy.andriy@gmail.com>
2024-04-22 12:08:56 +0300

    starter user model

--- ForumProject/ForumProject/settings.py
+++ ForumProject/ForumProject/settings.py
@@ -236,6 +236,9 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
 try:
     from .local_settings import *
 except ImportError:
