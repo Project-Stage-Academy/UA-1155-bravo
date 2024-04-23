7ac95be9f545b51b66ddeed3cae3dbebd392b589
Дарій Густаво Баутіста-Перепелиця <bautistadariy@gmail.com>
2024-04-23 11:19:58 +0300

    #36 created documentation strings

--- ForumProject/projects/serializers.py
+++ ForumProject/projects/serializers.py
@@ -4,6 +4,18 @@ from django.core.exceptions import ValidationError
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Project model.
+
+    Attributes:
+        id (int): The ID of the project (read-only).
+        project_name (str): The name of the project.
+        project_status (str): The status of the project.
+        startup (Startup): The startup associated with the project.
+    
+    Note: This Serializer is not finished yet. 
+    """    
+    
     class Meta:
         model = Project
         fields = ['id', 'project_name', 'project_status', 'startup']
@@ -12,6 +24,19 @@ class ProjectSerializer(serializers.ModelSerializer):
         
     
     def validate_project_name(self, value):
+        """
+        Validate the uniqueness of the project name for the given startup.
+
+        Parameters:
+            value (str): The project name to validate.
+
+        Returns:
+            str: The validated project name.
+
+        Raises:
+            serializers.ValidationError: If a project with the same name already exists for the startup.
+        """
+        
         if Project.objects.filter(startup=self.initial_data['startup'], project_name=value).exists():
             raise serializers.ValidationError("A project with this name already exists for the given startup.")
         return value    
--- ForumProject/startups/apps.py
+++ ForumProject/startups/apps.py
@@ -2,5 +2,12 @@ from django.apps import AppConfig
 
 
 class StartupsConfig(AppConfig):
+    """
+    Configuration class for the 'startups' app.
+
+    Attributes:
+        default_auto_field (str): The default auto field to use for models in this app.
+        name (str): The name of the app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'startups'
