a93cb6607874bdbd583f63ab7722c5c5f7e85848
Andrewakiv <danchyshy.andriy@gmail.com>
2024-04-23 15:34:11 +0300

    simple starter tests

--- ForumProject/users/tests.py
+++ ForumProject/users/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class PostTests(APITestCase):
+
+    def test_register_user(self):
+        """
+        Ensure we can create a new user.
+        """
+
+        data = {
+            'first_name': 'Martin',
+            'last_name': 'Daniels',
+            'email': 'daniels@gmail.com',
+            'password': 'Rootroot1$',
+            'password2': 'Rootroot1$',
+            'phone_number': '+380974131765'
+        }
+
+        expected_data = {
+            'id': 1,
+            'first_name': 'Martin',
+            'last_name': 'Daniels',
+            'email': 'daniels@gmail.com',
+            'phone_number': '+380974131765'
+        }
+        url = reverse('users:register')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, expected_data)
+
+    def test_register_user_invalid(self):
+        data = {
+            'first_name': 'Martin',
+            'last_name': 'Daniels',
+            'email': 'daniels@gmail.com',
+            'password': 'Rootroot1$',
+            'phone_number': '+380974131765'
+        }
+
+        url = reverse('users:register')
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
