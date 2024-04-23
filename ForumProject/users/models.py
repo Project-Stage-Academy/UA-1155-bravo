0317a1314df5acf2b7e7e7b8daf65353759a6495
Andrewakiv <danchyshy.andriy@gmail.com>
2024-04-23 14:04:10 +0300

    docstring for the CustomUser model

--- ForumProject/users/models.py
+++ ForumProject/users/models.py
@@ -11,7 +11,15 @@ class CustomUserManager(BaseUserManager):
 
     def create_user(self, email, password, **extra_fields):
         """
-        Create and save a User with the given email and password.
+        Create and save a user with the given email, password.
+
+        Parameters:
+        - email (str): The email address of the user.
+        - password (str): The password of the user.
+        - **extra_fields(dict): Other information.
+
+        Returns:
+        - CustomUser: The newly created user instance.
         """
         if not email:
             raise ValueError('The Email must be set')
@@ -23,7 +31,15 @@ class CustomUserManager(BaseUserManager):
 
     def create_superuser(self, email, password, **extra_fields):
         """
-        Create and save a SuperUser with the given email and password.
+        Create and save a superuser with the given email and password.
+
+        Parameters:
+        - email (str): The email address of the user.
+        - password (str): The password of the user.
+        - **extra_fields(dict): Other information.
+
+        Returns:
+        - CustomUser: The newly created user instance.
         """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -37,6 +53,31 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    """
+    A custom user model for authentication.
+
+    This model represents a user in the system, using email as the unique identifier.
+
+    Fields:
+    - id (BigAutoField): The primary key of the user.
+    - first_name (CharField): The first name of the user.
+    - last_name (CharField): The last name of the user.
+    - email (EmailField): The email address of the user (unique).
+    - phone_number (CharField): The phone number of the user.
+    - is_active (BooleanField): A flag indicating whether the user account is active.
+    - is_staff (BooleanField): A flag indicating whether the user has staff access.
+    - is_superuser (BooleanField): A flag indicating whether the user is a superuser.
+
+    Manager:
+    - objects: The default manager for the CustomUser model.
+
+    Attributes:
+    - USERNAME_FIELD: The field used as the unique identifier for authentication.
+    - REQUIRED_FIELDS: The list of fields required when creating a user.
+
+    Methods:
+    - __str__: Return a string representation of the user.
+    """
     id = models.BigAutoField(primary_key=True)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
@@ -51,4 +92,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     def __str__(self):
+        """
+        Return a string representation of the user.
+
+        Returns:
+        - str: The string representation of the user.
+        """
         return self.email
