f4c312f7a930c701f5a93ca8c7dbaf9f836429dd
Oleksandr Liashenko <o.i.liashenko@gmail.com>
2024-04-23 16:09:50 +0300

    users update, login template added

--- ForumProject/users/urls.py
+++ ForumProject/users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.throttling import ScopedRateThrottle
 
 from . import views
@@ -8,4 +8,7 @@ app_name = 'users'
 urlpatterns = [
     path('token/', views.TokenObtainPairView.as_view(throttle_classes=[ScopedRateThrottle]), name='token_obtain_pair'),
     path('token/refresh/', views.TokenRefreshView.as_view(throttle_classes=[ScopedRateThrottle]), name='token_refresh'),
+    # path('login/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('password_recovery/', views.PasswordRecoveryView.as_view(), name='password_recovery'),
 ]
--- ForumProject/users/views.py
+++ ForumProject/users/views.py
@@ -1,3 +1,8 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django_rest_passwordreset.serializers import PasswordTokenSerializer
+from .models import CustomUser
 from rest_framework_simplejwt.views import (TokenObtainPairView as BaseTokenObtainPairView,
                                             TokenRefreshView as BaseTokenRefreshView)
 
@@ -8,3 +13,35 @@ class TokenObtainPairView(BaseTokenObtainPairView):
 
 class TokenRefreshView(BaseTokenRefreshView):
     throttle_scope = 'token_refresh'
+
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('login')  # Redirect to logged-in page
+        else:
+            # Authentication failed, handle error
+            pass
+    return render(request, 'login.html')
+
+
+# from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+#
+# class PasswordRecoveryView(APIView):
+#     def post(self, request, *args, **kwargs):
+#         serializer = PasswordTokenSerializer(data=request.data)
+#         if serializer.is_valid():
+#             email = serializer.validated_data.get('email')
+#             try:
+#                 user = get_user_model().objects.get(email=email)
+#                 user.send_password_reset_email(request)
+#                 return Response({'detail': 'Password reset email has been sent.'}, status=status.HTTP_200_OK)
+#             except ObjectDoesNotExist:
+#                 pass
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
