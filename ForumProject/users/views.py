c545c2ca2eac20f8acc7b81d25670e9f8cc359d3
Dmitry <xedos0901@gmail.com>
2024-04-24 09:03:06 +0300

    Specific exception handling added

--- ForumProject/users/views.py
+++ ForumProject/users/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import AuthenticationFailed
 
 class TokenObtainPairView(BaseTokenObtainPairView):
     throttle_scope = 'token_obtain'
@@ -23,6 +24,12 @@ class LogoutView(APIView):
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response({"message": "User successfully logged out."}, status=status.HTTP_200_OK)
-        except Exception as e:
+        except TokenError:
             return Response({"error": "Invalid token."}, status=status.HTTP_400_BAD_REQUEST)
+        except KeyError:
+            return Response({"error": "Refresh token not provided."}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            # Log the unexpected error for further investigation
+            print(f"Unexpected error: {e}")
+            return Response({"error": "An unexpected error occurred."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
