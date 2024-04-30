6a3c21ba8c4879270489895b7d67cc2cbc41a5de
Dmitry <xedos0901@gmail.com>
2024-04-30 12:53:43 +0300

    Some changes

--- ForumProject/users/tests.py
+++ ForumProject/users/tests.py
@@ -1,39 +1,28 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken
-import json
+from users.models import CustomUser
 
 class LogoutTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.user_data = {
-            "email": "test@example.com",
-            "password": "testpassword",
-        }
-        # Create user
-        self.client.post(reverse("users:create"), self.user_data, content_type="application/json")
-        # Get access
-        response = self.client.post(reverse("token_obtain_pair"), self.user_data, content_type="application/json")
-        self.access_token = response.data["access"]
-        self.refresh_token = response.data["refresh"]
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = CustomUser.objects.create_user(email='test@gmail.com', password='Pa88word')
+        cls.user.is_active = True
+        cls.user.save()
+        cls.url_token = reverse('users:token_obtain_pair')
+        cls.url_logout = reverse('users:logout')
+
+        client = Client()
+        # Sending a request to obtain access tokens
+        response = client.post(cls.url_token, {'email': cls.user.email, 'password': 'Pa88word'}, content_type="application/json")
+        cls.access_token = response.data.get("access")
+        cls.refresh_token = response.data.get("refresh")
 
     def test_logout_success(self):
-        url = reverse("logout")
         headers = {
-            "Authorization": f"Bearer {self.refresh_token}",
+            "Authorization": f"Bearer {self.access_token}",
             "Content-Type": "application/json",
         }
-        response = self.client.post(url, content_type="application/json", **headers)
+        response = self.client.post(self.url_logout, content_type="application/json", **headers)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["message"], "User successfully logged out.")
-
-    def test_logout_invalid_token(self):
-        url = reverse("logout")
-        headers = {
-            "Authorization": "Bearer invalid_token",
-            "Content-Type": "application/json",
-        }
-        response = self.client.post(url, content_type="application/json", **headers)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data["error"], "Invalid token.")
