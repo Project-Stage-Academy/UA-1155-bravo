83f69372ef1d00b82991a4adac38b87a470b6122
Oleksandr Liashenko <o.i.liashenko@gmail.com>
2024-05-03 15:59:33 +0300

    initial commit

--- ForumProject/startups/models.py
+++ ForumProject/startups/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from .validation import phone_regex, image_validator
 from django_countries.fields import CountryField
+from notifications.models import Notification
 
 
 class Startup(models.Model):
@@ -26,6 +27,7 @@ class Startup(models.Model):
     startup_address = models.CharField(max_length=150)
     startup_logo = models.ImageField(upload_to='media/startup_logos/', validators=[image_validator], null=True, blank=True)
 
+
     def __str__(self):
         """
         Return the name of the startup.
@@ -46,3 +48,18 @@ class Startup(models.Model):
         
         if not self.startup_name or not self.startup_industry or not self.startup_phone or not self.startup_city or not self.startup_address:
             raise ValidationError("All fields must be filled in: name, industry, phone, country, city, address.")
+
+class NotificationPreference(models.Model):
+    """
+    Model representing notification preferences for each type.
+
+    Attributes:
+        startup (ForeignKey): Reference to the startup associated with the notification preferences.
+        notification_type (str): The type of notification.
+        email_notification (bool): Whether email notifications are enabled for the notification type.
+        in_app_notification (bool): Whether in-app notifications are enabled for the notification type.
+    """
+    startup = models.OneToOneField('Startup', on_delete=models.CASCADE, related_name='notification_preferences')
+    notification_type = models.CharField(max_length=50, choices=Notification.TRIGGER_CHOICES)
+    email_notification = models.BooleanField(default=True)
+    in_app_notification = models.BooleanField(default=True)
