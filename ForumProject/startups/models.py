4d11085bf6fa400c10c267d9afc5a3c8bee20c14
Oleksandr Liashenko <o.i.liashenko@gmail.com>
2024-05-04 12:23:41 +0300

    notifications settengs added

--- ForumProject/startups/models.py
+++ ForumProject/startups/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from .validation import phone_regex, image_validator
 from django_countries.fields import CountryField
-from notifications.models import Notification
 
 
 class Startup(models.Model):
@@ -17,6 +16,8 @@ class Startup(models.Model):
         startup_city (str): The city where the startup is located.
         startup_address (str): The address of the startup.
         startup_logo (ImageField): The logo of the startup.
+        email_notifications (bool): Indicates whether the startup wishes to receive notifications via email.
+        in_app_notifications (bool): Indicates whether the startup wishes to receive notifications within the app.
     """
     
     startup_name = models.CharField(max_length=150, unique=True) 
@@ -26,7 +27,8 @@ class Startup(models.Model):
     startup_city = models.CharField(max_length=50)
     startup_address = models.CharField(max_length=150)
     startup_logo = models.ImageField(upload_to='media/startup_logos/', validators=[image_validator], null=True, blank=True)
-
+    email_notifications = models.BooleanField(default=True)
+    in_app_notifications = models.BooleanField(default=True)
 
     def __str__(self):
         """
@@ -48,18 +50,3 @@ class Startup(models.Model):
         
         if not self.startup_name or not self.startup_industry or not self.startup_phone or not self.startup_city or not self.startup_address:
             raise ValidationError("All fields must be filled in: name, industry, phone, country, city, address.")
-
-class NotificationPreference(models.Model):
-    """
-    Model representing notification preferences for each type.
-
-    Attributes:
-        startup (ForeignKey): Reference to the startup associated with the notification preferences.
-        notification_type (str): The type of notification.
-        email_notification (bool): Whether email notifications are enabled for the notification type.
-        in_app_notification (bool): Whether in-app notifications are enabled for the notification type.
-    """
-    startup = models.OneToOneField('Startup', on_delete=models.CASCADE, related_name='notification_preferences')
-    notification_type = models.CharField(max_length=50, choices=Notification.TRIGGER_CHOICES)
-    email_notification = models.BooleanField(default=True)
-    in_app_notification = models.BooleanField(default=True)
