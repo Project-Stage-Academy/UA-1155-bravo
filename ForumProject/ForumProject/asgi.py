a3f9caa4503293f70a924dc3b9f519c850315589
Andrewakiv <danchyshy.andriy@gmail.com>
2024-05-07 03:40:59 +0300

    starter channels settings

--- ForumProject/ForumProject/asgi.py
+++ ForumProject/ForumProject/asgi.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ForumProject.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  'http': get_asgi_application(),
+})
--- ForumProject/ForumProject/settings.py
+++ ForumProject/ForumProject/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,7 +53,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
-
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -85,6 +86,15 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ForumProject.wsgi.application'
+ASGI_APPLICATION = 'ForumProject.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 
 # Database
