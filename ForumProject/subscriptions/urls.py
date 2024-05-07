6ae0c393251cc2b7b55372f5014b2ebf39c18523
Дарій Густаво Баутіста-Перепелиця <bautistadariy@gmail.com>
2024-05-07 14:31:17 +0300

    DRY principles, reworked view sets

--- ForumProject/subscriptions/urls.py
+++ ForumProject/subscriptions/urls.py
@@ -14,8 +14,7 @@ from . import views
 app_name = 'subscriptions'
 
 urlpatterns = [
-    path('add/', views.AddSubscription.as_view({'get': 'list', 'post': 'create', }), name='add-subscriptions'),
-    path('my/', views.ListMySubscription.as_view({'get': 'list'}), name='my-subscriptions'),
-    path('my/<int:pk>/', views.UniqueSubscription.as_view({'get': 'subscription_by_id', 'delete': 'delete'}), name='subscription-detail'),
-
+    path('add/', views.AddSubscription.as_view({'get': 'list', 'post': 'create'}), name='add-subscriptions'),
+    path('my/', views.SubscriptionViewsets.as_view({'get': 'list'}), name='my-subscriptions'),
+    path('my/<int:pk>/', views.SubscriptionViewsets.as_view({'get': 'subscription_by_id', 'delete': 'delete'}), name='subscription-detail'),
 ]
--- ForumProject/subscriptions/views.py
+++ ForumProject/subscriptions/views.py
@@ -13,6 +13,7 @@ from rest_framework.permissions import IsAuthenticated
 from users.permissions import IsInvestorCompanySelected
 from .filters import MySubscriptionFilter
 from rest_framework.pagination import PageNumberPagination
+from django.core.paginator import EmptyPage
 
 
 class CustomPagination(pagination.PageNumberPagination):
@@ -22,14 +23,33 @@ class CustomPagination(pagination.PageNumberPagination):
     page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 100
-    
+
+    def paginate_queryset(self, queryset, request, view=None):
+        """
+        Custom pagination to handle invalid page numbers.
+        """
+        try:
+            return super().paginate_queryset(queryset, request, view=view)
+        except Exception as e:
+            return self.handle_invalid_page_number(str(e))
+
+    def handle_invalid_page_number(self, error_message):
+        """
+        Handle scenarios where the page number is invalid.
+        """
+        return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_page(self, *args, **kwargs):
+        try:
+            return super().get_page(*args, **kwargs)
+        except EmptyPage:
+            return self.handle_invalid_page_number("Page does not exist.")
 
 class AddSubscription(viewsets.ModelViewSet):
     """
     View for adding a subscription.
 
     """
-
     serializer_class = SubscribeInvestorStartupSerializer
     pagination_class = CustomPagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
@@ -46,6 +66,22 @@ class AddSubscription(viewsets.ModelViewSet):
         """
         return Startup.objects.all()
 
+    def filter_queryset(self, queryset):
+        """
+        Filter the queryset and handle any invalid filter input.
+        """
+        try:
+            queryset = super().filter_queryset(queryset)
+        except Exception as e:
+            return self.handle_invalid_filters(str(e))
+        return queryset
+
+    def handle_invalid_filters(self, error_message):
+        """
+        Handle scenarios where filters have invalid input or encounter errors.
+        """
+        return Response({'error': error_message}, status=status.HTTP_400_BAD_REQUEST)
+
 
     def list(self, request, *args, **kwargs):
         """
@@ -54,8 +90,10 @@ class AddSubscription(viewsets.ModelViewSet):
         Returns:
             Response: Paginated list of startups.
         """
-        ...
         queryset = self.filter_queryset(self.get_queryset())
+        if queryset.count() == 0:
+            return Response({'error': 'No startups found with the given filters.'}, status=status.HTTP_404_NOT_FOUND)
+
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = StartupSerializer(page, many=True)
@@ -89,15 +127,41 @@ class AddSubscription(viewsets.ModelViewSet):
         else:
             return Response({'error': 'User is not associated with an investor'}, status=status.HTTP_400_BAD_REQUEST)
 
-
-class ListMySubscription(viewsets.ReadOnlyModelViewSet):
+class SubscriptionMixin:
     """
-    View for listing user's subscriptions.
+    Mixin class containing methods related to user's subscriptions.
+    """
 
-    Serializer class: SubscribeInvestorStartupSerializer
-    Filter backends: DjangoFilterBackend, SearchFilter
-    Filterset class: MySubscriptionFilter
-    Search fields: startup__startup_name, startup__startup_industry, startup__startup_country
+    def get_user_investor(self):
+        """
+        Get the user's investor profile.
+
+        Returns:
+            UserInvestor or None: Investor profile if exists, None otherwise.
+        """
+        user = self.request.user
+        try:
+            user_investor = UserInvestor.objects.get(customuser=user)
+            return user_investor.investor
+        except UserInvestor.DoesNotExist:
+            return None
+
+    def get_subscriptions_by_investor(self, user_investor):
+        """
+        Get subscriptions related to the given investor profile.
+
+        Args:
+            user_investor (UserInvestor): User's investor profile.
+
+        Returns:
+            Queryset: Subscriptions related to the user's investor profile.
+        """
+        return SubscribeInvestorStartup.objects.filter(investor=user_investor)
+
+
+class SubscriptionViewsets(viewsets.ModelViewSet, SubscriptionMixin):
+    """
+    View for retrieving, and deleting a unique subscription.
     """
     serializer_class = SubscribeInvestorStartupSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
@@ -112,63 +176,18 @@ class ListMySubscription(viewsets.ReadOnlyModelViewSet):
         Returns:
             Queryset: Subscriptions related to the user's investor profile.
         """
-        user = self.request.user
-        try:
-            user_investor = UserInvestor.objects.get(customuser=user)
-            investor_id = user_investor.investor_id
-            queryset = SubscribeInvestorStartup.objects.filter(investor_id=investor_id)
-            return queryset
-        except UserInvestor.DoesNotExist:
+        user_investor = self.get_user_investor()
+        if user_investor:
+            return self.get_subscriptions_by_investor(user_investor)
+        else:
             return SubscribeInvestorStartup.objects.none()
 
-    def list(self, request, *args, **kwargs):
-        """
-        List user's subscriptions with startup details.
-
-        Returns:
-            Response: List of subscriptions with startup details.
-        """
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-        response_data = []
-        for subscription in serializer.data:
-            startup_id = subscription['startup']
-            startup = Startup.objects.get(id=startup_id)
-            startup_serializer = StartupSerializer(startup)
-            subscription['startup'] = startup_serializer.data
-            response_data.append(subscription)
-        return Response(response_data)
-    
-# consider the possibility of choosing the investor profile that will be subscribed
-
-
-class UniqueSubscription(viewsets.ModelViewSet):
-    """
-    View for retrieving, and deleting a unique subscription.
-    """
-
-    serializer_class = SubscribeInvestorStartupSerializer
-    # permission_classes = [IsInvestorCompanySelected]
-
-    def get_queryset(self):
-        """
-        Get the queryset for user's subscriptions.
-
-        Returns:
-            Queryset: Subscriptions related to the user's investor profile.
-        """
-        user = self.request.user
-        try:
-            user_investor = UserInvestor.objects.get(customuser=user)
-            investor_id = user_investor.investor_id
-            return SubscribeInvestorStartup.objects.filter(investor_id=investor_id)
-        except UserInvestor.DoesNotExist:
-            return SubscribeInvestorStartup.objects.none()
-
-    @action(detail=True, methods=['get'])
-    def subscription_by_id(self, request, pk=None):
+    def get_subscription_with_startup_details(self, pk):
         """
         Retrieve a subscription by its ID with startup details.
+
+        Args:
+            pk: Primary key of the subscription.
 
         Returns:
             Response: Subscription details with startup details.
@@ -182,18 +201,51 @@ class UniqueSubscription(viewsets.ModelViewSet):
             return Response(subscription_data)
         except SubscribeInvestorStartup.DoesNotExist:
             return Response({'error': 'Subscription not found'}, status=status.HTTP_404_NOT_FOUND)
-        
+
+    @action(detail=True, methods=['get'])
+    def subscription_by_id(self, request, pk=None):
+        """
+        Retrieve a subscription by its ID with startup details.
+
+        Returns:
+            Response: Subscription details with startup details.
+        """
+        return self.get_subscription_with_startup_details(pk)
     
+    def list(self, request, *args, **kwargs):
+        """
+        List all subscriptions with startup details.
 
-def delete(self, request, pk, format=None):
-    """
-    Delete a subscription by its ID.
+        Returns:
+            Response: List of subscriptions with startup details.
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+        if queryset.count() == 0:
+            return Response({'error': 'No startups found with the given filters.'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = self.get_serializer(queryset, many=True)
+        response_data = []
+        for subscription in serializer.data:
+            startup_id = subscription['startup']
+            startup = Startup.objects.get(id=startup_id)
+            startup_serializer = StartupSerializer(startup)
+            subscription['startup'] = startup_serializer.data
+            response_data.append(subscription)
+        return Response(response_data)
 
-    Returns:
-        Response: Success message if subscription is deleted.
-    """
-    subscription = self.get_object()
-    subscription.delete()
-    return Response({'message': 'Unsubscribed successfully.'}, status=status.HTTP_204_NO_CONTENT)
-  
-  
+    def delete(self, request, pk=None, format=None):
+        """
+        Delete a subscription by its ID.
+
+        Returns:
+            Response: Success message if subscription is deleted.
+        """
+        try:
+            subscription = self.get_object()
+            subscription.delete()
+            return Response({'message': 'Unsubscribed successfully.'}, status=status.HTTP_204_NO_CONTENT)
+        except SubscribeInvestorStartup.DoesNotExist:
+            return Response({'error': 'Subscription not found'}, status=status.HTTP_404_NOT_FOUND)
+        except AttributeError:
+            return Response({'error': 'Subscription ID is missing'}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception:
+            return Response({'error': 'Please select a unique startup to unsubscribe'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
