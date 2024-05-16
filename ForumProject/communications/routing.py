29fdd41c5d5bee31f22e71a4a555de25b161125b
Oleksandr Liashenko <o.i.liashenko@gmail.com>
2024-05-16 17:24:13 +0300

    routing update

--- ForumProject/communications/routing.py
+++ ForumProject/communications/routing.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from .consumers import ChatNotificationConsumer
 from . import consumers
 
 websocket_urlpatterns = [
     path('ws/chat/<str:room_name>/', consumers.ChatConsumer.as_asgi()),
+    path('ws/chat_notifications/', ChatNotificationConsumer.as_asgi()),
 ]
